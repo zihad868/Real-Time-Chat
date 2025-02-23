3ee1db35d2730b1ce2b9af1251b6b70681f2c436
zihad868 <zihadh868@gmail.com>
2025-02-23 16:25:09 +0600

    mongodb connection

--- Chat App/server/index.js
+++ Chat App/server/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
 const app = express();
-const port = 5000;
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cors());
+
+const port =  process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_DB_URI;
 
 
 app.get('/', (req, res) =>{
@@ -11,3 +20,15 @@ app.get('/', (req, res) =>{
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
+
+
+// mongoose.connect(URI,{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => {
+//     console.log("MongoDB connection established")
+// })
+
+mongoose.connect(MONGO_URI).then(() => {
+    console.log("MongoDB connection established")
+});
